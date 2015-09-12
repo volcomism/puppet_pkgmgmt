6fb49308016107009703e31ee414c68cd6e14da7
volcomism <anfany@anfany.com>
2015-09-12 12:28:39 +0200

    fixed class name

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@
 #
 # This module manages packages
 #
-class devops_puppet_pkgmgmt {
+class puppet_pkgmgmt {
     # hieralookup for sources list
     $hiera_sources = hiera_hash('apt_sources', {})
     create_resources('apt::source', $hiera_sources)
