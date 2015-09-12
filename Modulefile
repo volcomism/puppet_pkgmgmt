6e783230962c969d960eeef29501c6aa3c6dc5b0
volcomism <anfany@anfany.com>
2015-09-12 10:23:23 +0200

    added wrapper

---
+++ Modulefile
@@ -0,0 +1,8 @@
+name         'puppet_pkgmgmt'
+version      '0.0.1'
+license      'NA'
+summary      'puppet_pkgmgmt'
+description  'installs and removes packages'
+
+## Add dependencies, if any:
+
---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+# == Class: puppet_pkgmgmt
+#
+# This module manages packages
+#
+class devops_puppet_pkgmgmt {
+    # hieralookup for sources list
+    $hiera_sources = hiera_hash('apt_sources', {})
+    create_resources('apt::source', $hiera_sources)
+    $hiera_packages = hiera_hash('packages', {})
+    create_resources('package', $hiera_packages)
+}
